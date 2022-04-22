1468bd132b1ab621beb351bbee760b186bb9cfc2
adeelkbw <adeelnasirkbw@gmail.com>
2022-04-23 01:11:01 +0500

    Contract compilation complete --contract

---
+++ compile.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const solc = require('solc');
+
+function compilation() {
+    const Lottery = fs.readFileSync('./contracts/Lottery.sol', "utf8")
+    let complierInput = {
+        language: 'Solidity',
+        sources:
+        {
+            'Lottery.sol':
+            {
+                content: Lottery
+            }
+        },
+        settings:
+        {
+            optimizer:
+            {
+                enabled: true
+            },
+            outputSelection:
+            {
+                '*': {
+                    '*': ['*']
+                }
+            }
+        }
+    };
+
+
+    console.log("Contract Compiling....");
+    const output = JSON.parse(solc.compile(JSON.stringify(complierInput)))
+    console.log("contract Complied Successfully");
+    console.log(output);
+    const outputContract = output.contracts['Lottery.sol']['lottery']
+    // console.log(outputContract);
+    module.exports.abi = outputContract.abi
+    module.exports.bytecode = outputContract.evm.bytecode.object
+
+
+}
+
+compilation()
